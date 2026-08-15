1b48ee047e1704ed4023b0efedfdae8732039bc5
fady <fadym11111@gmail.com>
2026-08-15 16:19:39 +0300

    Add Gemini API integration

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel , Field
+from google import genai
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = FastAPI()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
 # like schema validation like struct in c++
 class EmailRequest(BaseModel):
-    title: str
+    title: str = Field(min_length=3)
     purpose: str
-    tone: str = 'professional'
+    tone: str = Field(default='professional') 
     recipient: str
-    word_limit: int = 50
+    word_limit: int = Field(default=50, ge=20, le=500)
 
 @app.get('/')
 async def base():
@@ -23,4 +31,14 @@ async def genEmail(data:EmailRequest):
         "tone": data.tone,
         "recipient": data.recipient,
         "word_limit": data.word_limit
+    }
+
+@app.get('/test')
+async def test():
+    response = client.models.generate_content(
+        model="gemini-3.6-flash",
+        contents="Write a short professional email asking for vacation."
+    )
+    return {
+        "response": response.text
     }
