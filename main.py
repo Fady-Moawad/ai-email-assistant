d5798689755cb5e1c616bfebb5cd3e6deee50160
fady <fadym11111@gmail.com>
2026-08-15 17:20:22 +0300

    AI call in error boundary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel , Field
 from google import genai
 from dotenv import load_dotenv
@@ -17,34 +17,55 @@ class EmailRequest(BaseModel):
     purpose: str
     tone: str = Field(default='professional') 
     recipient: str
-    word_limit: int = Field(default=50, ge=20, le=500)
+    word_limit: int = Field(default=50, ge=20, le=100)
 
 @app.get('/')
 async def base():
     return {"message": "AI Email Assistant API"}
 
 @app.post('/generate-email')
-async def genEmail(data:EmailRequest):
-    prompt = f"""
-    Write an email based on the following requirements:
+async def genEmail(data: EmailRequest):
+    # AI call must in error boundary
+    try:
+        # Structured Output With Prompt
+        prompt = f"""
+Write an email based on the following requirements:
 
-    Title: {data.title}
-    Purpose: {data.purpose}
-    Tone: {data.tone}
-    Recipient: {data.recipient}
-    Word limit: {data.word_limit}
+Title: {data.title}
+Purpose: {data.purpose}
+Tone: {data.tone}
+Recipient: {data.recipient}
+Word limit: {data.word_limit}
 
-    Return only the email.
-    """
-    response = client.models.generate_content(
-        model="gemini-3.6-flash",
-        contents=prompt
-    )
-    return {
-        "success": True,
-        "email": response.text
-    }
+Return the result as JSON with exactly these fields:
 
+{{
+    "subject": "...",
+    "greeting": "...",
+    "body": "...",
+    "closing": "..."
+}}
+
+Do not add any other fields.
+"""
+
+        response = client.models.generate_content(
+            model="gemini-3.6-flash",
+            contents=prompt
+        )
+
+        return {
+            "success": True,
+            "email": response.text
+        }
+
+    except Exception:
+       raise HTTPException ( 
+            status_code=500,
+            detail="Failed to generate email")
+
+            
+        
 @app.get('/test')
 async def test():
     response = client.models.generate_content(
